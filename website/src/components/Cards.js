61f21da28f66cfb3bc6087e09178ff6d3d894090
Kenny Cain <kjc23@st-andrews.ac.uk>
2024-03-06 21:46:33 +0000

    website

---
+++ website/src/components/Cards.js
@@ -0,0 +1,51 @@
+import React from "react";
+import CardItem from "./CardItem";
+import "./Cards.css"
+
+export default function Cards() {
+    return (
+        <div className="cards">
+            <h1>Upcoming Competitions</h1>
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                        <CardItem
+                        src="images/img-9.jpg"
+                        text="Explore the hidden waterfall deep inside
+                        the Amazon Jungle"
+                        label="Adventure"
+                        path="/services"
+                        />
+                        <CardItem
+                        src="images/img-2.jpg"
+                        text="Travel through the Island of Bali"
+                        label="Luxury"
+                        path="/services"
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem
+                        src="images/img-1.jpg"
+                        text="Explore the hidden waterfall deep inside
+                        the Amazon Jungle"
+                        label="Adventure"
+                        path="/services"
+                        />
+                        <CardItem
+                        src="images/img-3.jpg"
+                        text="Travel through the Island of Bali"
+                        label="Luxury"
+                        path="/services"
+                        />
+                        <CardItem
+                        src="images/img-4.jpg"
+                        text="Travel through the Island of Bali"
+                        label="Luxury"
+                        path="/services"
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ website/src/components/HeroSection.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./HeroSection.css";
+import { Button } from "./Button";
+import "../App.css";
+
+
+export default function HeroSection() {
+    return(
+        <div className="hero-container">
+            <video src="/videos/edited_body_building.mp4" autoPlay loop muted />
+            <h1>AMATEUR BODY BUILDERS OF SCOTLAND</h1>
+            <p></p>
+            <div className="hero-btns">
+                <Button className="btns" buttonStyle="btn--outline"
+                buttonSize="btn--large">GET STARTED
+                </Button>
+                <Button className="btns" buttonStyle="btn--primary"
+                buttonSize="btn--large">WATCH TRAILER <i className="far fa-play-circle" />
+                </Button>
+            </div>
+        </div>
+    )
+}
