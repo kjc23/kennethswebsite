4a8283e02375dd3dad1d4309ef113e7ca1ca2794
Kenny Cain <kjc23@st-andrews.ac.uk>
2025-02-03 19:39:16 +0000

    studies

--- src/components/pages/StudyHome.js
+++ src/components/pages/StudyHome.js
@@ -17,7 +17,9 @@ export default function StudyHome(props) {
         { name: "Micah 6", fileName: "Micah 6", book: "Micah" },
         { name: "Mark 1:16-39", fileName: "Mark 1 16-39", book: "Mark"},
         { name: "Mark 2:18-3:35", fileName: "Mark218-335", book: "Mark"},
-        { name: "Mark 4:35-5:43", fileName: "Mark 4 35-5 43", book: "Mark"}
+        { name: "Mark 4:35-5:43", fileName: "Mark 4 35-5 43", book: "Mark"},
+        { name: "Mark 6:53-7:23", fileName: "Mark 6 53-7 23", book: "Mark"},
+        { name: "Mark 9:1-29", fileName: "Mark 9 1-29", book: "Mark"}
     ];
 
     return (
